14a9971ce8585c8079206bf6c75d9d521b50e9f0
michelleconville <michelle.conville@gmail.com>
2023-01-12 21:52:31 +0000

    Add main function and random word function

--- run.py
+++ run.py
@@ -1,4 +1,22 @@
+import random
+
 import colorama
 from colorama import Fore, Style
 
 colorama.init(autoreset=True)
+
+def get_random_word():
+    """
+    Picks a random word from words.txt 
+    """
+    random_word = random.choice(open("words.txt", "r").read().split('\n'))
+
+    return get_random_word()
+
+def main():
+    """
+    Runs the game 
+    """
+    print("Lets get started")
+    
+main()
